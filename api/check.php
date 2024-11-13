9c99873ca8914c519e7b1e7cc04d4b5468f6ef12
Douxx <136928935+douxxu@users.noreply.github.com>
2024-11-13 20:41:14 +0100

    Removed major security issue
    Thanks to @judemont to reporting this issue 
    If we did "google.com | acommand" the command would be executed

--- api/check.php
+++ api/check.php
@@ -1,25 +1,36 @@
 <?php
 function isSiteUp($url) {
+    $parsedUrl = parse_url($url, PHP_URL_HOST) ?? $url;
+    $domain = filter_var($parsedUrl, FILTER_VALIDATE_DOMAIN, FILTER_FLAG_HOSTNAME);
+
+    if ($domain === false) {
+        return [
+            'status' => 'error',
+            'message' => 'Invalid URL provided.'
+        ];
+    }
+
     $output = null;
     $statusCode = null;
-    exec("ping -c 1 -W 5 $url", $output, $statusCode); 
-    
+
+    exec("ping -c 1 -W 5 " . escapeshellarg($domain), $output, $statusCode);
+
     if ($statusCode === 0) {
         preg_match('/time=(\d+\.?\d*) ms/', implode("\n", $output), $matches);
         $pingTime = $matches[1];
-        
+
         if ($pingTime > 5000) {
             return [
                 'status' => 'down',
                 'message' => 'The response time is too long.'
             ];
         }
-        
-        $headers = get_headers("http://$url", 1);
+
+        $headers = @get_headers("http://$domain", 1);
         return [
             'status' => 'up',
             'ping' => $pingTime,
-            'headers' => $headers
+            'headers' => $headers ?: 'Unable to retrieve headers'
         ];
     } else {
         return [
